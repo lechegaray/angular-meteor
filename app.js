9b7bf758d3d7a2ac0d3e3d50e337796e5da11990
Luis Echegaray <leche@z65-50-122-229.ips.direcpath.com>
2015-10-07 23:35:15 -0400

    added a collection, and seed data

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
   angular.module('socially', ['angular-meteor']);
  
-  angular.module('socially').controller('PartiesListCtrl', ['$scope', function ($scope) {
-    $scope.parties = [
-      {
-        'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.'
-      },
-      {
-        'name': 'All dubstep all the time',
-        'description': 'Get it on!'
-      },
-      {
-        'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.'
-      }
-    ];
+  angular.module('socially').controller('PartiesListCtrl', ['$scope', '$meteor', function ($scope, $meteor) {
+    $scope.parties = $meteor.collection(Parties);
   }]);
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Parties.find().count() === 0) {
+      var parties = [
+        {'name': 'Dubstep-Free Zone',
+          'description': 'Fast just got faster with Nexus S.'},
+        {'name': 'All dubstep all the time',
+          'description': 'Get it on!'},
+        {'name': 'Savage lounging',
+          'description': 'Leisure suit required. And only fiercest manners.'}
+      ];
+      for (var i = 0; i < parties.length; i++)
+        Parties.insert(parties[i]);
+    }
+  });
 }
