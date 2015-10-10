9ee045dfa854603b7d4ea3953b13cda720bb0ecf
Luis Echegaray <mrluisechegaray@gmail.com>
2015-10-09 22:35:59 -0400

    added privacy, and user specific view on data, removed autopublish and used pub/sub

--- client/parties/controllers/partiesDetails.js
+++ client/parties/controllers/partiesDetails.js
@@ -1,17 +1,5 @@
 angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteor',
     function($scope, $stateParams, $meteor){
-        $scope.party = $meteor.object(Parties, $stateParams.partyId);
-
-        $scope.save = function(){
-            $scope.party.save().then(function(numberOfDocs){
-                console.log('save success doc affected ', numberOfDocs);
-            }, function(error){
-                console.log('save error', error);
-            });
-        };
-
-        $scope.reset = function(){
-            $scope.party.reset();
-        };
-
+        $scope.party = $meteor.object(Parties, $stateParams.partyId).subscribe('parties');
+        $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
     }]);
